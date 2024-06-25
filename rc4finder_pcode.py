db5ba46025dc9d2ba9ce9230e209a7582ab75bc5
OpaxIV <93701325+OpaxIV@users.noreply.github.com>
2024-06-25 13:16:07 +0200

    still working

--- rc4finder_pcode.py
+++ rc4finder_pcode.py
@@ -135,19 +135,13 @@ def xorCheck(func):
 # hexCheck(function)
 # Description: Checks if given function contains at any point the hex value 0x100
 def hexCheck(func):
-    constant = 0x100
     addrSet = func.getBody()
     opiter = listing.getInstructions(addrSet, True)             # True means forward
     for op in opiter:
         raw_pcode = op.getPcode()
-        #raw_pcode_str = ''.join([str(p) for p in raw_pcode])    # ghidra won't output it correctly
-        for p in raw_pcode:
-            for input in p.getInputs():
-                #print("Input:", input, "Type:", type(input), "Offset:", input.getOffset())  # debugging
-                #print(input)
-                if input.isConstant():
-                    if constant in input:
-                        return True
+        raw_pcode_str = ''.join([str(p) for p in raw_pcode])    # ghidra won't output it correctly
+        if '0x100' in raw_pcode_str:
+            return True
     return False
 
 
